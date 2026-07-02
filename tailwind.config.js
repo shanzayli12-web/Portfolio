f9bda6a5f0dceaa1d34195270cc6683aff6bfec9
shanzayli12-web <shanzayli12@gmail.com>
2026-07-02 15:40:59 +0500

    Update typography to Lora & Satoshi

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,8 +33,8 @@ export default {
         }
       },
       fontFamily: {
-        sans: ['"Inter"', 'sans-serif'],
-        editorial: ['"General Sans"', 'sans-serif'],
+        sans: ['"Satoshi"', '"Inter"', 'sans-serif'],
+        editorial: ['"Lora"', 'serif'],
       }
     },
   },
